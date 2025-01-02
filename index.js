f0f2e12db31ec5a17fbbcd13d3d001c35fcf3e8d
Renjish-k-s <renjishksimon@gmail.com>
2025-01-02 23:00:11 +0530

    designing

---
+++ index.js
@@ -0,0 +1,8 @@
+function get_register() {
+    document.getElementById('registartion').style.display = 'flex';
+   // alert('hello');
+}
+function go_back() {
+    document.getElementById('registartion').style.display = 'none';
+   // alert('hello');
+}
